be24bcb7fad1ceefc4be394b8bcaf76cb7df7a88
Andre Cesconetto <begcesconetto2@gmail.com>
2013-06-01 16:22:45 -0300

    Relatorio

--- MMC2/Controllers/RelatorioController.cs
+++ MMC2/Controllers/RelatorioController.cs
@@ -17,70 +17,82 @@ namespace MMC2.Controllers
 
         private MHCAEntities db = new MHCAEntities();
 
-        public ActionResult Index()
+        public ActionResult Index(int projetoid = 0)
         {
+            var obj = (from a in db.Projetos select new { a.Id, a.Nome }).ToList();
+            ViewBag.Projeto = obj;
+            Session["-IDPROJETO"] = projetoid;
             return View();
         }
+
         public ActionResult Relatorio(int _id = 0)
         {
-            LocalReport localReport = new LocalReport();
-            localReport.ReportPath = Server.MapPath("~/Report/ReportRelatorioProjeto.rdlc");
-            var obj = (from a in db.vw_relatorio_projeto
-                       where a.projeto_id == _id
-                       select new
-                       {
-                           a.projeto_id,
-                           a.projeto,
-                           a.DescricaoProjeto,
-                           a.cliente,
-                           a.DtIniProjeto,
-                           a.DtFimProjeto,
-                           a.tarefa,
-                           a.DescricaoTarefa,
-                           a.tarefa_id,
-                           a.colaborador,
-                           a.status,
-                           a.datalancamento,
-                           a.qtdhoras,
-                           a.DescricaoHistorico,
-                           a.historico_id
-                       });
-            ReportDataSource reportDataSource = new ReportDataSource("DataSet1", obj);
-            localReport.DataSources.Add(reportDataSource);
+            _id = Convert.ToInt32(Session["-IDPROJETO"]);
+            if (_id > 0)
+            {
+                LocalReport localReport = new LocalReport();
+                localReport.ReportPath = Server.MapPath("~/Report/ReportRelatorioProjeto.rdlc");
+                var obj = (from a in db.vw_relatorio_projeto
+                           where a.projeto_id == _id
+                           select new
+                           {
+                               a.projeto_id,
+                               a.projeto,
+                               a.DescricaoProjeto,
+                               a.cliente,
+                               a.DtIniProjeto,
+                               a.DtFimProjeto,
+                               a.tarefa,
+                               a.DescricaoTarefa,
+                               a.tarefa_id,
+                               a.colaborador,
+                               a.status,
+                               a.datalancamento,
+                               a.qtdhoras,
+                               a.DescricaoHistorico,
+                               a.historico_id
+                           });
+                ReportDataSource reportDataSource = new ReportDataSource("DataSet1", obj);
+                localReport.DataSources.Add(reportDataSource);
 
-            string reportType = "PDF";
-            string mimeType;
-            string encoding;
-            string fileNameExtension;
+                string reportType = "PDF";
+                string mimeType;
+                string encoding;
+                string fileNameExtension;
 
-            //The DeviceInfo settings should be changed based on the reportType
-            //http://msdn2.microsoft.com/en-us/library/ms155397.aspx
-            string deviceInfo =
-            "<DeviceInfo>" +
-            "  <OutputFormat>PDF</OutputFormat>" +
-            "  <PageWidth>8.5in</PageWidth>" +
-            "  <PageHeight>11in</PageHeight>" +
-            "  <MarginTop>0.5in</MarginTop>" +
-            "  <MarginLeft>1in</MarginLeft>" +
-            "  <MarginRight>1in</MarginRight>" +
-            "  <MarginBottom>0.5in</MarginBottom>" +
-            "</DeviceInfo>";
+                //The DeviceInfo settings should be changed based on the reportType
+                //http://msdn2.microsoft.com/en-us/library/ms155397.aspx
+                string deviceInfo =
+                "<DeviceInfo>" +
+                "  <OutputFormat>PDF</OutputFormat>" +
+                "  <PageWidth>8.5in</PageWidth>" +
+                "  <PageHeight>11in</PageHeight>" +
+                "  <MarginTop>0.5in</MarginTop>" +
+                "  <MarginLeft>1in</MarginLeft>" +
+                "  <MarginRight>1in</MarginRight>" +
+                "  <MarginBottom>0.5in</MarginBottom>" +
+                "</DeviceInfo>";
 
-            Warning[] warnings;
-            string[] streams;
-            byte[] renderedBytes;
+                Warning[] warnings;
+                string[] streams;
+                byte[] renderedBytes;
 
-            //Render the report
-            renderedBytes = localReport.Render(
-                reportType,
-                deviceInfo,
-                out mimeType,
-                out encoding,
-                out fileNameExtension,
-                out streams,
-                out warnings);
-            //Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers." + fileNameExtension);
-            return File(renderedBytes, mimeType);
+                //Render the report
+                renderedBytes = localReport.Render(
+                    reportType,
+                    deviceInfo,
+                    out mimeType,
+                    out encoding,
+                    out fileNameExtension,
+                    out streams,
+                    out warnings);
+                //Response.AddHeader("content-disposition", "attachment; filename=NorthWindCustomers." + fileNameExtension);
+                return File(renderedBytes, mimeType);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
         }
 
     }
